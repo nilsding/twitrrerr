d65fbbbee8d0741bf3cddcb3aca7a3565cc2a5c9
nilsding <nilsding@nilsding.org>
2014-12-17 02:26:26 +0100

    "fixed" that hack (integer too big to convert to int)

--- lib/ext/qvariant.rb
+++ lib/ext/qvariant.rb
@@ -1,12 +1,12 @@
 # see: https://techbase.kde.org/Development/Languages/Ruby#Alternate_way_to_emit_Ruby_Classes
 class Object
   def to_variant
-    Qt::Variant.new object_id
+    Qt::Variant.new object_id.to_s
   end
 end
 
 class Qt::Variant
   def to_object
-    ObjectSpace._id2ref to_int
+    ObjectSpace._id2ref to_string.to_i
   end
 end
